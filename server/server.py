05af8cf7730df76319fe0949b9d72b2d1c77b063
Logeshwaran29 <logeshwarancse20@gmail.com>
2024-01-17 14:24:44 +0530

    new

--- server/server.py
+++ server/server.py
@@ -1,1 +1,21 @@
-import pymongo
+from flask import Flask,jsonify
+from pymongo import MongoClient
+from bson import json_util
+
+app=Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017')
+db=client.Tiruchengode
+collection=db.ksrEng
+
+@app.route('/')
+def index():
+
+    res=collection.find({'key':'admission details'})
+    output=[document.get('res',None) for document in res]
+    data=json_util.dumps(output)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
