ff44328b603a43c06d9074651878e9378c858cdb
Logeshwaran29 <logeshwarancse20@gmail.com>
2024-02-09 20:26:23 +0530

    new

--- client/src/components/admin.jsx
+++ client/src/components/admin.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './admin.css';
+import Select from 'react-select';
+import axios from 'axios';
 
 const admin = () =>{
+    const [selected,setSelected]=useState('all');
+
+    const handle = (event) => {
+        setSelected(event.target.value);
+    };
+
     return(
-        <div className="admin">admin</div>
+        <div className="admin">
+            <div className="drop-box">
+                {/* <select className="drop" name='options' onChange={handle}>
+                    <option className='option' value='all'>All</option>
+                    <option className='option' value='answer'>Answered</option>
+                    <option className='option' value='notAns'>Not Answered</option>
+                </select> */}
+                
+            </div>
+        </div>
     )
 }
 
