de0405563ebdc23da1d86be90b6d30de1ec52722
Logeshwaran29 <logeshwarancse20@gmail.com>
2024-02-17 20:57:53 +0530

    new

--- client/src/components/admin.jsx
+++ client/src/components/admin.jsx
@@ -7,24 +7,40 @@ const admin = () =>{
     const [selected,setSelected]=useState('all');
 
     const options = [
-        { value: 'option1', label: 'Option 1' },
-        { value: 'option2', label: 'Option 2' },
-        { value: 'option3', label: 'Option 3' },
+        { value: 'all', label: 'All' },
+        { value: 'ans', label: 'Answered' },
+        { value: 'nAns', label: 'Not Answered' },
     ];
 
     const handleSelectChange = (selectedOption) => {
         setSelected(selectedOption);
     };
 
+    const customStyles = {
+        control: (provided, state) => ({
+          ...provided,
+          border: '1px solid #ccc',
+          borderRadius: '4px',
+          boxShadow: state.isFocused ? '0 0 0 2px #3366cc' : null,
+        }),
+        option: (provided, state) => ({
+          ...provided,
+          backgroundColor: state.isSelected ? '#3366cc' : null,
+          color: state.isSelected ? 'white' : 'black',
+        }),
+      };
+
     return(
         <div className="admin">
             <div className="drop-box">
                     <Select
+                    className='select'
                     id="dropdown"
                     options={options}
                     value={selected}
                     onChange={handleSelectChange}
                     isSearchable={false}
+                    styles={customStyles}
                     />
             </div>
         </div>
