af3bde86340aa2d9a374177bffc3e72202bd14ac
Logeshwaran29 <logeshwarancse20@gmail.com>
2024-04-24 13:38:24 +0530

    new

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -74,7 +74,7 @@ const Chat = () => {
       setSuggestion([]);
       setloading(true);
 
-      await axios.post('http://192.168.1.5:5000/chat',{"query" : cInput.replace(/\./g, ' ').trim()})
+      await axios.post('https://chatbot-twrj.onrender.com/chat',{"query" : cInput.replace(/\./g, ' ').trim()})
       .then(response =>{
         setTimeout(() => {
           setChatMessages(prevMessages => [
